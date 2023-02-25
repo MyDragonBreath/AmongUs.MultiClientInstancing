85483fc29d0216c62e5a29de65004594a7313c79
MyDragonBreath <38029594+MyDragonBreath@users.noreply.github.com>
2023-02-25 11:57:58 +1100

    Bypass limit

--- MCI/Patches/KeyboardJoystick.cs
+++ MCI/Patches/KeyboardJoystick.cs
@@ -5,7 +5,7 @@ using System.Linq;
 namespace MCI.Patches
 {
     [HarmonyPatch(typeof(KeyboardJoystick), nameof(KeyboardJoystick.Update))]
-    class Keyboard_Joystick
+    public sealed class Keyboard_Joystick
     {
         public static int controllingFigure = 0;
         public static void Postfix()
@@ -13,7 +13,7 @@ namespace MCI.Patches
             if (Input.GetKeyDown(KeyCode.F5))
             {
                 controllingFigure = PlayerControl.LocalPlayer.PlayerId;
-                if (PlayerControl.AllPlayerControls.Count == 15) return; //remove this if your willing to suffer with the consequences. 
+                if (PlayerControl.AllPlayerControls.Count == 15 && !Input.GetKeyDown(KeyCode.F6)) return; //hold f6 and press f5 to bypass limit
                 Utils.CleanUpLoad();
                 Utils.CreatePlayerInstance("Robot");
             }
