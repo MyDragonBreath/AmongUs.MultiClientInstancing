b5af8c385aa14db935ec39c8e5b0ef36ffd4843e
MyDragonBreath <38029594+MyDragonBreath@users.noreply.github.com>
2023-02-28 18:16:49 +1100

    Countdown timer no more

--- MCI/MCIPlugin.cs
+++ MCI/MCIPlugin.cs
@@ -29,5 +29,16 @@ namespace MCI
 
 
         public static bool Persistence = true;
+        
+    }
+
+
+    [HarmonyPatch(typeof(GameStartManager), nameof(GameStartManager.Update))]
+    public static class CountdownPatch
+    {
+        public static void Prefix(GameStartManager __instance)
+        {
+            __instance.countDownTimer = 0;
+        }
     }
 }
