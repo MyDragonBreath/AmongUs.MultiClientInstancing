57493a47bfb611a81bd6e84ed543b5b817c5e149
lekillerdesgames <141348096+lekillerdesgames@users.noreply.github.com>
2023-08-04 20:10:54 +0200

    Update MCIPlugin.cs

--- MCI/MCIPlugin.cs
+++ MCI/MCIPlugin.cs
@@ -9,6 +9,7 @@ namespace MCI
     [BepInAutoPlugin("dragonbreath.au.mci", "MCI", VersionString)]
     [BepInProcess("Among Us.exe")]
     [BepInDependency(SubmergedCompatibility.SUBMERGED_GUID, BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency(ReactorPlugin.Id, BepInDependency.DependencyFlags.SoftDependency)]
     public partial class MCIPlugin : BasePlugin
     {
         public const string VersionString = "0.0.6";
